a6ebb20616690375d871223511583a0949d9278d
azkalrix <azkalrix@gmail.com>
2023-11-19 17:39:26 +0100

    reconfigure the file location

---
+++ javascriptexe.js
@@ -0,0 +1,26 @@
+let al = document.getElementById("count-el");
+let prescore = document.getElementById("prev");
+let num = 0;
+
+function increase(){
+    num += 1;
+    al.innerText = num;
+}
+
+function decrease(){
+    if (num>0){
+        num -= 1;
+        al.innerText = num;
+    }
+    else (alert ("Invalid Input"))
+}  
+
+function save(){
+    if (num>0){
+        let lave = num + "-";
+        prescore.textContent += lave;
+        al.innerText = 0;
+        num = 0
+    }
+    else (alert ("Invalid Input"))
+}
